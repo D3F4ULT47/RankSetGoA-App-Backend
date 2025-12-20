db46bbf178bedecd2f1e6cc3a233067777886044
BlueBerryBug <24je0220@iitism.ac.in>
2025-12-20 23:34:08 +0530

    changing the origin to allow netlify app

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Vite frontend
+    origin: "https://ranksetgo-app.netlify.app/", // Netlify frontend
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
